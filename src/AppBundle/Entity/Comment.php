b5764aba2253836af3f594efe5835c8fabf0a377
Ichinator <arnolddouwma@orange.fr>
2018-01-19 16:46:37 +0100

    FOSCommentBundle is integrate with FOSUserBundle

--- src/AppBundle/Entity/Comment.php
+++ src/AppBundle/Entity/Comment.php
@@ -5,12 +5,14 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
     /**
      * @ORM\Id
@@ -26,5 +28,32 @@ class Comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Thread")
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
+
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
 ?>
